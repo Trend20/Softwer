14c5e5fd4139209a18607b1a2c534ad57bde82b4
Trend20 <enockomondi305@gmail.com>
2021-02-04 17:57:42 +0300

    working on the cards page

--- src/components/pages/Cards/Cards.js
+++ src/components/pages/Cards/Cards.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './Cards.css';
 
 function Cards() {
     return (
-        <div>
-            <h1>Types of Cards</h1>
+        <div className="cards">
+            <div className="card-intro">
+                <div className="card-intro-text">
+                    <h1>Choose the card and package that suits you best</h1>
+                    <p>We will help you make a decision on what card to choose. Depending on your needs, our team is ready to give you a piece of advice.</p>
+                    <button>View all plans</button>
+                </div>
+
+                <div className="card-intro-img">
+                    <img src="/img/cardintro.png" alt="card"/>
+                </div>
+            </div>
         </div>
     )
 }
