08f0e5ec707449fed902996778e6dcf77c4be868
Trend20 <enockomondi305@gmail.com>
2021-02-04 12:20:02 +0300

    working on about page

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './About.css';
 
 function About() {
     return (
-        <div>
-            <h1>About</h1>
+        <div className="about">
+            <div className="about-intro">
+                <div className="intro-text">
+                    <h1>Application that simplify your life</h1>
+                    <p>Tired of puzzling out how to track your charges or plan your finances? Our application introduces you the simplest and 
+                        softwerest way to start taking control of your money.</p>
+                        <button>Open bank account</button>
+                </div>
+
+                <div className="intro-image">
+                    <img src="/img/intro.png" alt="intro" />
+                </div>
+            </div>
         </div>
     )
 }
