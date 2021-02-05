ae029d76bd4b7916a1959f08a6a672d0eeaea0cc
Trend20 <enockomondi305@gmail.com>
2021-02-05 14:21:48 +0300

    Updates

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './pages/Home/Home';
-export { default as About } from './pages/About/About';
-export { default as Cards } from './pages/Cards/Cards';
-export { default as Plans } from './pages/Plans/Plans';
-export { default as Registration } from './pages/Registration/Registration';
