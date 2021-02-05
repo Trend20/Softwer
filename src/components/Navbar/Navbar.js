dbf1f6c72e8b7c3eeade58537acd0493a84a57ea
Trend20 <enockomondi305@gmail.com>
2021-02-05 12:38:18 +0300

    fixing header routes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import { BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
 
 import { Home, About, Cards, Plans, Registration} from "../index";
 
+
 // icons
-import { FaBars } from 'react-icons/fa';
+import { ReactComponent as CloseMenu } from "../../assets/x.svg";
+import { ReactComponent as MenuIcon } from "../../assets/menu.svg";
+import logo from '../../assets/logo.png';
 
 
 // navbar styles
@@ -14,14 +18,19 @@ import './Navbar.css';
 // logo image
 
 function Navbar() {
+
+    const [ click, setClick ] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
     return (
         <Router>
-            <div className="navbar">
+            <div className="header">
 
                 {/* logo  */}
 
-                <Link to="/" className="logo">
-                    <img src="/img/logo.png" alt="logo" />
+                {/* <Link to="/" className="logo">
+                    <img src={logo} alt="logo" />
                 </Link>
                 <nav>
                     <ul>
@@ -45,9 +54,56 @@ function Navbar() {
                             <Link to="/registration">Registration</Link>
                         </li>
                     </ul>
-                </nav>
+                </nav> */}
+                  <div className="logo-nav">
+                    <div className="logo-container">
+                       <Link to="/">
+                         <a href="#">
+                             <img src={logo} alt="logo" />
+                         </a>
+                       </Link>
+                    </div>
+                    <ul className={click ? "nav-options active" : "nav-options"}>
 
-                <i className="toggle"> <FaBars /> </i>
+                        <li className="option" onClick={closeMobileMenu}>
+                           <Link to="/">Home</Link>
+                        </li>
+
+                        <li className="option" onClick={closeMobileMenu}>
+                           <Link to="/about">About</Link>
+                        </li>
+
+                        <li className="option" onClick={closeMobileMenu}>
+                           <Link to="/cards">Types of Cards</Link>
+                        </li>
+
+                        <li className="option mobile-option" onClick={closeMobileMenu}>
+                           <Link to="/plans">Plans</Link>
+                        </li>
+                        
+                        <li className="option mobile-option" onClick={closeMobileMenu}>
+                           <Link to="/registration<">Registration</Link>
+                        </li>
+                    </ul>
+                 </div>
+
+                 <ul className="signin-up">
+                    <li className="sign-in" onClick={closeMobileMenu}>
+                        <a href="#">SIGN-IN</a>
+                    </li>
+                    <li onClick={closeMobileMenu}>
+                        <a href="" className="signup-btn">
+                           SIGN-UP
+                        </a>
+                    </li>
+                 </ul>
+                 <div className="mobile-menu" onClick={handleClick}>
+                    {click ? (
+                     <CloseMenu className="menu-icon" />
+                    ) : (
+                     <MenuIcon className="menu-icon" />
+                     )}
+                 </div>
             </div>
 
              {/* switch the routes */}
