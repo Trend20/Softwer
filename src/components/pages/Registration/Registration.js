db2a7a7ca47ac36fb737b9d55b86ac348f93e0d4
Trend20 <enockomondi305@gmail.com>
2021-05-05 13:33:53 +0300

    page navigations

--- src/components/pages/Registration/Registration.js
+++ src/components/pages/Registration/Registration.js
@@ -1,46 +1,41 @@
-import React from 'react'
+import React from 'react';
 import './Registration.css';
 
 import Footer from '../../Footer/Footer';
 
 function Registration() {
-    return (
-        <div className="account">
-            <form id="form">
-                <h1>Personal Information</h1>
-                <p>To get started, tell us about yourself.</p>
+	return (
+		<div className="account">
+			<form id="form">
+				<h1>Personal Information</h1>
+				<p>To get started, tell us about yourself.</p>
 
-                <div className="form-fields">
-                    <div className="form-group">
-                        <label for="firstname">First name</label>
-                        <input type="text" placeholder="First name*" />
-                    </div>
-                    <div className="form-group">
-                        <label for="lastname">Last name</label>
-                        <input type="text" placeholder="Last name*" />
-                    </div>
-                    <div className="form-group">
-                        <label for="email">Email address</label>
-                        <input type="email" placeholder="Email address*" />
-                    </div>
+				<div className="form-fields">
+					<div className="form-group">
+						<input type="text" placeholder="First name*" />
+					</div>
+					<div className="form-group">
+						<input type="text" placeholder="Last name*" />
+					</div>
+					<div className="form-group">
+						<input type="email" placeholder="Email address*" />
+					</div>
 
-                    <div className="form-group">
-                        <label for="Day/Month/Year">Day/Month/Year</label>
-                        <input type="text" placeholder="Day/Month/Year" />
-                    </div>
+					<div className="form-group">
+						<input type="text" placeholder="Day/Month/Year" />
+					</div>
 
-                    <div className="form-group">
-                        <label for="message">Message</label>
-                        <textarea placeholder="Message*" cols="20" rows="10"></textarea>
-                    </div>
-                </div>
+					<div className="form-group">
+						<textarea placeholder="Message*" cols="20" rows="10"></textarea>
+					</div>
+				</div>
 
-                <button>Registration Now</button>
-            </form>
+				<button>Registration Now</button>
+			</form>
 
-            <Footer />
-        </div>
-    )
+			<Footer />
+		</div>
+	);
 }
 
-export default Registration
+export default Registration;
