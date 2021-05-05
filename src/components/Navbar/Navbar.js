02854f10fcfc63f1deb0bf566899bb1ed08e0ad8
Trend20 <enockomondi305@gmail.com>
2021-05-05 13:24:43 +0300

    page navigations

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-import { BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
-import { Home, About, Cards, Plans, Registration} from "../index";
-
+import { Home, About, Cards, Plans, Registration } from '../index';
 
 // icons
-import { ReactComponent as CloseMenu } from "../../assets/x.svg";
-import { ReactComponent as MenuIcon } from "../../assets/menu.svg";
+import { ReactComponent as CloseMenu } from '../../assets/x.svg';
+import { ReactComponent as MenuIcon } from '../../assets/menu.svg';
 import logo from '../../assets/logo.png';
-
 
 // navbar styles
 
@@ -18,109 +16,125 @@ import './Navbar.css';
 // logo image
 
 function Navbar() {
+	const [click, setClick] = useState(false);
+	const handleClick = () => setClick(!click);
+	const closeMobileMenu = () => setClick(false);
 
-    const [ click, setClick ] = useState(false);
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+	return (
+		<Router>
+			<div className="header">
+				{/* logo  */}
+				<div className="logo-nav">
+					<div className="logo-container">
+						<Link to="/">
+							<a href="#">
+								<img src={logo} alt="logo" />
+							</a>
+						</Link>
+					</div>
+					<ul className={click ? 'nav-options active' : 'nav-options'}>
+						<li className="option" onClick={closeMobileMenu}>
+							<Link
+								to="/"
+								style={{
+									textDecoration: 'none',
+									fontSize: '.9rem',
+									fontWeight: '700',
+								}}
+							>
+								Home
+							</Link>
+						</li>
 
-    return (
-        <Router>
-            <div className="header">
+						<li className="option" onClick={closeMobileMenu}>
+							<Link
+								to="/about"
+								style={{
+									textDecoration: 'none',
+									fontSize: '.9rem',
+									fontWeight: '700',
+								}}
+							>
+								About
+							</Link>
+						</li>
 
-                {/* logo  */}
-                  <div className="logo-nav">
-                    <div className="logo-container">
-                       <Link to="/">
-                         <a href="#">
-                             <img src={logo} alt="logo" />
-                         </a>
-                       </Link>
-                    </div>
-                    <ul className={click ? "nav-options active" : "nav-options"}>
+						<li className="option" onClick={closeMobileMenu}>
+							<Link
+								to="/cards"
+								style={{
+									textDecoration: 'none',
+									fontSize: '.9rem',
+									fontWeight: '700',
+								}}
+							>
+								Types of Cards
+							</Link>
+						</li>
 
-                        <li className="option" onClick={closeMobileMenu}>
-                           <Link to="/" style={{
-                               textDecoration: 'none',
-                               fontSize: '1.2rem',
-                               fontWeight: '700'
-                           }}>Home</Link>
-                        </li>
+						<li className="option" onClick={closeMobileMenu}>
+							<Link
+								to="/plans"
+								style={{
+									textDecoration: 'none',
+									fontSize: '.9rem',
+									fontWeight: '700',
+								}}
+							>
+								Plans
+							</Link>
+						</li>
 
-                        <li className="option" onClick={closeMobileMenu}>
-                           <Link to="/about" style={{
-                               textDecoration: 'none',
-                               fontSize: '1.2rem',
-                               fontWeight: '700'
-                           }}>About</Link>
-                        </li>
+						<li className="option" onClick={closeMobileMenu}>
+							<Link
+								to="/registration"
+								style={{
+									textDecoration: 'none',
+									fontSize: '.9rem',
+									fontWeight: '700',
+								}}
+							>
+								Registration
+							</Link>
+						</li>
+					</ul>
+				</div>
 
-                        <li className="option" onClick={closeMobileMenu}>
-                           <Link to="/cards" style={{
-                               textDecoration: 'none',
-                               fontSize: '1.2rem',
-                               fontWeight: '700'
-                           }}>Types of Cards</Link>
-                        </li>
+				<div className="mobile-menu" onClick={handleClick}>
+					{click ? <CloseMenu className="menu-icon" /> : <MenuIcon className="menu-icon" />}
+				</div>
+			</div>
 
-                        <li className="option" onClick={closeMobileMenu}>
-                           <Link to="/plans"
-                           style={{
-                            textDecoration: 'none',
-                            fontSize: '1.2rem',
-                            fontWeight: '700'
-                        }}>Plans</Link>
-                        </li>
+			{/* switch the routes */}
 
-                        <li className="option" onClick={closeMobileMenu}>
-                           <Link to="/registration" style={{
-                               textDecoration: 'none',
-                               fontSize: '1.2rem',
-                               fontWeight: '700'
-                           }}>Registration</Link>
-                        </li>
-                    </ul>
-                 </div>
+			<Switch>
+				{/* renders home page */}
+				<Route exact path="/">
+					<Home />
+				</Route>
 
-                 <div className="mobile-menu" onClick={handleClick}>
-                    {click ? (
-                     <CloseMenu className="menu-icon" />
-                    ) : (
-                     <MenuIcon className="menu-icon" />
-                     )}
-                 </div>
-            </div>
+				{/* renders about page */}
+				<Route path="/about">
+					<About />
+				</Route>
 
-             {/* switch the routes */}
+				{/* renders types of cards page */}
+				<Route path="/cards">
+					<Cards />
+				</Route>
 
-                <Switch>
+				{/* renders plans page */}
+				<Route path="/plans">
+					<Plans />
+				</Route>
 
-                    {/* renders home page */}
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                     
-                    {/* renders about page */}
-                    <Route path="/about">
-                        <About />
-                    </Route>
-
-                     {/* renders types of cards page */}
-                    <Route path="/cards">
-                        <Cards />
-                    </Route>
-
-                    {/* renders plans page */}
-                    <Route path="/plans">
-                        <Plans />
-                    </Route>
-
-                    {/* renders registration page */}
-                    <Route path="/registration">
-                        <Registration/>
-                    </Route>
-                </Switch>
-        </Router>
-    )
+				{/* renders registration page */}
+				<Route path="/registration">
+					<Registration />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
-export default Navbar
+export default Navbar;
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,115 +1,128 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Home.css';
 
-import { FaFacebook, FaTwitter, FaLinkedin,} from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import { MdCastConnected } from 'react-icons/md';
 import Footer from '../../Footer/Footer';
 
 function Home() {
-    return (
-        <div className="home-page">
-            <div className="hero">
-                <div className="hero-text">
-                    <h5>The mobile bank</h5>
-                    <h1>The world at your fingertips</h1>
-                    <button className="button">Open bank account</button>
-                </div>
+	return (
+		<div className="home-page">
+			<div className="hero">
+				<div className="hero-text">
+					<h5>The mobile bank</h5>
+					<h1>The world at your fingertips</h1>
+					<Link to="/registration" className="button">
+						Open bank account
+					</Link>
+				</div>
 
-                <div className="hero-slides">
-                        <img src="/img/hero-mg1.png" alt="slide1" />
-                </div>
-            </div>
+				<div className="hero-slides">
+					<img src="/img/hero-mg1.png" alt="slide1" />
+				</div>
+			</div>
 
+			{/* card section */}
 
-            {/* card section */}
+			<div className="card">
+				<div className="card-text">
+					<h3>Take control of your finances. With just one app.</h3>
+					<p className="card-description">
+						No more worries about your cash expenses. With the help of our application, you will have the
+						opportunity to check and regulate your finances anytime you want.
+					</p>
+				</div>
 
-            <div className="card">
-                <div className="card-text">
-                    <h3>Take control of your finances. With just one app.</h3>
-                    <p className="card-description">
-                      No more worries about your cash expenses. With the help of our application, you will have the opportunity to check and regulate your finances anytime you want.
-                    </p>
-                </div>
+				<div className="card-image">
+					<img src="/img/card.png" alt="card" />
+				</div>
+			</div>
 
-                <div className="card-image">
-                    <img src="/img/card.png" alt="card" />
-                </div>
-            </div>
+			{/* travel section */}
 
-            {/* travel section */}
+			<div className="travel">
+				<div className="travel-img">
+					<img src="/img/travel.png" alt="travel" />
+				</div>
 
-            <div className="travel">
-                <div className="travel-img">
-                    <img src="/img/travel.png" alt="travel" />
-                </div>
+				<div className="travel-text">
+					<h1>Travel like you never left</h1>
+					<h5>Spend anywhere</h5>
+					<p>Feel the unique advantages of using the application in every corner of the world</p>
+					<h5>Get up and go</h5>
+					<p>Wherever you are, you are one click apart from making any purchase you want</p>
+				</div>
+			</div>
 
-                <div className="travel-text">
-                    <h1>Travel like you never left</h1>
-                    <h5>Spend anywhere</h5>
-                    <p>Feel the unique advantages of using the application in every corner of the world</p>
-                    <h5>Get up and go</h5>
-                    <p>Wherever you are, you are one click apart from making any purchase you want</p>
-                </div>
-            </div>
+			{/* account info*/}
 
-            {/* account info*/}
+			<div className="account-demo">
+				<div className="demo-text">
+					<h1>Get clarity over your money, in ways you`d never expect</h1>
+					<p>
+						In case you ever feel worried about the safety of your accounts, our application will give you
+						completely clear report based on your charges.
+					</p>
+				</div>
 
-            <div className="account-demo">
-                <div className="demo-text">
-                    <h1>Get clarity over your money, in ways you`d never expect</h1>
-                    <p>
-                      In case you ever feel worried about the safety of your accounts, our application will give you completely clear report based on your charges.
-                    </p>
-                </div>
+				<div className="demo-image">
+					<img src="/img/demo.png" alt="demo" />
+				</div>
+			</div>
 
-                <div className="demo-image">
-                    <img src="/img/demo.png" alt="demo" />
-                </div>
-            </div>
+			{/* chat section */}
 
-            {/* chat section */}
+			<div className="chat">
+				<div className="chat-image">
+					<img src="/img/chatbg.png" />
+				</div>
 
-            <div className="chat">
-                <div className="chat-image">
-                    <img src="/img/chatbg.png" />
-                </div>
+				<div className="chat-text">
+					<h1>Chat us, tweet us, email us</h1>
+					<p>
+						An experienced team of friendly and reliable technical support specialists will help you in
+						solving complicated and urgent questions.
+					</p>
 
-                <div className="chat-text">
-                    <h1>Chat us, tweet us, email us</h1>
-                    <p>
-                      An experienced team of friendly and reliable technical support specialists will help you in solving complicated and urgent questions.
-                    </p>
+					<img src="/img/chat.png" alt="chat" />
 
-                    <img src="/img/chat.png"  alt="chat"/>
+					<div className="social">
+						<div className="head">
+							<p>Follow Us:</p>
+						</div>
 
-                    <div className="social">
+						<div className="icons">
+							<i>
+								<MdCastConnected />
+							</i>
+							<i>
+								<FaFacebook />
+							</i>
+							<i>
+								<FaTwitter></FaTwitter>
+							</i>
+							<i>
+								<FaLinkedin />
+							</i>
+						</div>
+					</div>
+				</div>
+			</div>
 
-                       <div className="head">
-                            <p>Follow Us:</p>
-                        </div>
+			{/* sign-up */}
+			<div className="sign-up">
+				<h1>Sign up and get your bank account in 8 minutes</h1>
+				<p>Sign up right now and enjoy the wide variety of possibilities which our application provides.</p>
+				<Link to="/registration" className="button" style={{ width: '30%' }}>
+					Open bank account
+				</Link>
+			</div>
 
-                        <div className="icons">
-                           <i><MdCastConnected /></i>
-                           <i><FaFacebook /></i>
-                           <i><FaTwitter ></FaTwitter></i>
-                           <i><FaLinkedin /></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            {/* sign-up */}
-            <div className="sign-up">
-                <h1>Sign up and get your bank account in 8 minutes</h1>
-                <p>Sign up right now and enjoy the wide variety of possibilities which our application provides.</p>
-                <button className="button">Open bank account</button>
-            </div>
-
-            {/* footer */}
-            <Footer />
-
-        </div>
-    )
+			{/* footer */}
+			<Footer />
+		</div>
+	);
 }
 
-export default Home
+export default Home;
